93922576bafb98cf18362ebb36c41ed785755a5a
hw <39850866+Judge1234@users.noreply.github.com>
2019-02-16 11:21:59 -0500

    Update auto.py

--- PySmash/backend/auto.py
+++ PySmash/backend/auto.py
@@ -1,13 +1,15 @@
-def auto_0(driver, url):
-    driver.get(url)
-    first_name  = ''
-    last_name   = ''
-    email       = ''
-    subject     = ''
-    message     = ''
-    submit      = ''
+class Smasher:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+        self.fields = {}
 
+    def start(self):
+        self.driver.get(self.url)
 
-
-    
+    def quit(self):
+        self.driver.close()
  
+
+    def __repr__(self):
+        return f'Smasher: {self.url}'
